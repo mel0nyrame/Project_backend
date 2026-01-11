15f758f9fb3ffc5f41796dccc6746a35e3e7686c
Mel0ny <2189829539@qq.com>
2026-01-11 23:12:36 +0800

    错误捕获器

---
+++ src/main/java/com/mel0ny/springboot/exception/GlobalExceptionHandler.java
@@ -0,0 +1,17 @@
+package com.mel0ny.springboot.exception;
+
+import com.mel0ny.springboot.pojo.Result;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+@Slf4j
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(Exception.class)
+    public Result exceptionHandler(Exception exception) {
+        log.error("发生错误",exception);
+        return Result.error();
+    }
+}
