f4a33de081c4507d494806b6267677e0fd62b2c0
Mel0ny <2189829539@qq.com>
2026-01-12 00:10:48 +0800

    严格按照springmvc模式

--- src/main/java/com/mel0ny/springboot/controller/StudentController.java
+++ src/main/java/com/mel0ny/springboot/controller/StudentController.java
@@ -2,6 +2,7 @@ package com.mel0ny.springboot.controller;
 
 import com.mel0ny.springboot.mapper.StudentMapper;
 import com.mel0ny.springboot.pojo.Result;
+import com.mel0ny.springboot.service.StudentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,10 +13,10 @@ import org.springframework.web.bind.annotation.RestController;
 public class StudentController {
 
     @Autowired
-    public StudentMapper studentMapper;
+    private StudentService studentService;
 
     @GetMapping("/")
     public Result allUser() {
-        return Result.success(studentMapper.allStudent());
+        return Result.success(studentService.getAllStudent());
     }
 }
---
+++ src/main/java/com/mel0ny/springboot/service/StudentService.java
@@ -0,0 +1,20 @@
+package com.mel0ny.springboot.service;
+
+import com.mel0ny.springboot.mapper.StudentMapper;
+import com.mel0ny.springboot.pojo.Student;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class StudentService {
+
+    @Autowired
+    private StudentMapper studentMapper;
+
+    public List<Student> getAllStudent() {
+        return studentMapper.allStudent();
+    }
+
+}
