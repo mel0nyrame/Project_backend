bc61ce9a8b7a45808e9a6991b44e44c5681cc353
Mel0ny <2189829539@qq.com>
2026-01-11 23:40:50 +0800

    基本的controller

---
+++ src/main/java/com/mel0ny/springboot/controller/StudentController.java
@@ -0,0 +1,21 @@
+package com.mel0ny.springboot.controller;
+
+import com.mel0ny.springboot.mapper.StudentMapper;
+import com.mel0ny.springboot.pojo.Result;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/student")
+public class StudentController {
+
+    @Autowired
+    public StudentMapper studentMapper;
+
+    @GetMapping("/")
+    public Result allUser() {
+        return Result.success(studentMapper.allStuent());
+    }
+}
